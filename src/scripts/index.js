5064459a9820d7cd66705588aa026f3d6c8ee1c8
TurnerHelical <hunterde91@gmail.com>
2025-07-23 09:57:06 -0400

    working on adding a function to run on resize to prep the page for the new size

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,13 @@
 import "../CSS/reset.css";
 import "../CSS/style.css";
 import {Page} from "./page.js";
+import {Dom} from "./utils.js"
 
+const utils = new Dom();
 const onLoad = new Page();
 
 onLoad.init();
+window.addEventListener('resize',() => {
+    
+});
+;
